6316e3229abc9665bfe26c3e1d5ea062e78441c2
Ray Thomas <mhsbari@gmail.com>
2018-06-23 22:18:15 -0400

    Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,20 @@ module.exports = {
     title: 'Gatsby + Netlify CMS Starter',
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-121301765-1",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+      },
+    },
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     {
