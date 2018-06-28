492b1516333b4eaf95f7865012a4cd08697f4d01
Ray Thomas <mhsbari@gmail.com>
2018-06-27 23:48:27 -0400

    Add RSS feed link, and move links on navbar to the right side.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,24 +2,30 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 import logo from '../img/logo.svg'
+import rss from '../img/rss-feed.svg'
 
 const Navbar = () => (
   <nav className="navbar is-dark">
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
-          <object class="icon is-large" type="image/svg+xml" data={logo}></object>
+          <object class="image is-48x48" type="image/svg+xml" data={logo}></object>
           <h1 className="title has-text-white">
             FPGA Pro
           </h1>
         </Link>
       </div>
       <div className="navbar-start">
+      </div>
+      <div className="navbar-end">
         <Link className="navbar-item has-text-white" to="/about">
           About
         </Link>
-      </div>
-      <div className="navbar-end">
+        <span className="navbar-item">
+          <a href="/rss.xml" className="">
+              <img src={rss} className="image is-24x24" />
+          </a>
+        </span>
       </div>
     </div>
   </nav>
